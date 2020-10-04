81e0fab9e3e3b9fcc0b99808646aa409084ece59
Edwin Norell <edwinnorell@hotmail.com>
2020-10-04 20:59:29 +0200

    fan

--- src/views/home/Home.tsx
+++ src/views/home/Home.tsx
@@ -15,7 +15,7 @@ const _Home: React.FunctionComponent<HomeProps> = (props): JSX.Element => {
   const [number, setNumber] = useState<number>(0);
 
   const increment = () => {
-    setNumber(number + 1);
+    setNumber(number + 12000);
   };
 
   const decrement = () => {
